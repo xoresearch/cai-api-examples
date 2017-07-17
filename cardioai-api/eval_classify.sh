76d275d239743d4fa0f6f36172590fc461da2dca
danek-kulik <danek.kulik@gmail.com>
2017-07-17 17:49:21 +0300

    updated eval_classify example: new ECG record

--- cardioai-api/eval_classify.sh
+++ cardioai-api/eval_classify.sh
@@ -4,7 +4,7 @@ curl \
 	--verbose \
 	--request POST \
 	--header "Content-Type: application/json; charset=utf-8" \
-	--data-binary "@./eval_classify_request.json" \
+	--data-binary "@./eval_classify_easi.json" \
 	--output "./eval_classify_response.json" \
 	"https://api.cardio.ai/v1/evaluation/classify"
 
