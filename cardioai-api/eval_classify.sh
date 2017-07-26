b5d8f97851dd62e07582b3d91b64ace99c6d3783
danek.kulik <danek.kulik@gmail.com>
2017-07-26 22:55:55 +0300

    fixed eval_classify.sh script

--- cardioai-api/eval_classify.sh
+++ cardioai-api/eval_classify.sh
@@ -5,7 +5,7 @@ curl \
 	--max-time 60 \
 	--request POST \
 	--header "Content-Type: application/json; charset=utf-8" \
-	--data-binary "@./std12_indian3.json" \
+	--data-binary "@./std12_10min.json" \
 	--output "./eval_classify_response.json" \
 	"https://api.cardio.ai/v1/evaluation/classify"
 
