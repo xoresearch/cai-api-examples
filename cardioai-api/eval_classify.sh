c334ab9041c1470e49724800d6d35dd944161d90
danek-kulik <danek.kulik@gmail.com>
2017-08-16 21:36:10 +0300

    update for examples

--- cardioai-api/eval_classify.sh
+++ cardioai-api/eval_classify.sh
@@ -5,5 +5,5 @@ curl \
 	--max-time 60 \
 	--request POST \
 	--header "Content-Type: application/json; charset=utf-8" \
-	--data-binary "@./std12_10min.json" \
+	--data-binary "@./std12_gen.json" \
 	"https://api.cardio.ai/v1/evaluation/classify"
