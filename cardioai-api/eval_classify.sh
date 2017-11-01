a3d154e983d1f115427e534fd1b45f3e0ea97bec
danek-kulik <danek.kulik@gmail.com>
2017-11-01 16:49:59 +0200

    updated examples for the new API

--- cardioai-api/eval_classify.sh
+++ cardioai-api/eval_classify.sh
@@ -5,5 +5,5 @@ curl \
 	--max-time 60 \
 	--request POST \
 	--header "Content-Type: application/json; charset=utf-8" \
-	--data-binary "@./std12_gen.json" \
+	--data-binary "@./demorec_easi_5min.json" \
 	"https://api.cardio.ai/v1/evaluation/classify"
--- cardioai-api/eval_plot.sh
+++ cardioai-api/eval_plot.sh
@@ -5,6 +5,6 @@ curl \
 	--max-time 60 \
 	--request POST \
 	--header "Content-Type: application/json; charset=utf-8" \
-	--data-binary "@./std12_gen.json" \
+	--data-binary "@./demorec_easi_5min.json" \
 	--output "./eval_plot_response.png" \
 	"https://api.cardio.ai/v1/evaluation/plot"
