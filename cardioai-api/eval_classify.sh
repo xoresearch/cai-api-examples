c459c2812875fc8f98bb042382d95b7c7f6ce06a
danek-kulik <danek.kulik@gmail.com>
2017-12-20 07:12:05 +0200

    update for examples

--- cardioai-api/eval_classify.sh
+++ cardioai-api/eval_classify.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+#	"https://api.cardio.ai/v1/evaluation/classify"
+
 curl \
 	--verbose \
 	--max-time 60 \
 	--request POST \
+	--header "X-Api-Key: 6dtjQ3y5jJj9Pw8sfsiNPNu2lTzaR1Vh9qtHQOKN1WIongydH-2VfHbBj9AG-ebjCJSypT7FA0LF" \
 	--header "Content-Type: application/json; charset=utf-8" \
 	--data-binary "@./demorec_std12_5min.json" \
-	"https://api.cardio.ai/v1/evaluation/classify"
+	"http://192.168.80.102:50100/v1/evaluation/classify"
