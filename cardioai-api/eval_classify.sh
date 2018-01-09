60f748d3a292f94c1e982dc41781f6965a60d5c3
danek-kulik <danek.kulik@gmail.com>
2018-01-09 17:46:06 +0200

    updated examples for the new API

--- cardioai-api/eval_classify.sh
+++ cardioai-api/eval_classify.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-#	"https://api.cardio.ai/v1/evaluation/classify"
-
 curl \
 	--verbose \
 	--max-time 60 \
 	--request POST \
-	--header "X-Api-Key: cr6JEi37Z1KRBnTBhmEWy_GuSOW5hKKgn2XFEYpCS8Zwyw6Umxuq6pPBO3wGSS5pdfnHrd4erWuB" \
+	--header "X-Api-Key: your-api-key" \
 	--header "Content-Type: application/json; charset=utf-8" \
-	--data-binary "@./demorec_std12_5min.json" \
-	"http://192.168.80.102:50100/v1/evaluation/classify"
+	--data-binary "@./demorec_easi_5min.json" \
+	"https://api.cardio.ai/v1/evaluation/classify"
--- cardioai-api/eval_plot.sh
+++ cardioai-api/eval_plot.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-#	"https://api.cardio.ai/v1/evaluation/plot"
-
 curl \
 	--verbose \
 	--max-time 60 \
 	--request POST \
-	--header "X-Api-Key: cr6JEi37Z1KRBnTBhmEWy_GuSOW5hKKgn2XFEYpCS8Zwyw6Umxuq6pPBO3wGSS5pdfnHrd4erWuB" \
+	--header "X-Api-Key: your-api-key" \
 	--header "Content-Type: application/json; charset=utf-8" \
-	--data-binary "@./demorec_std12_5min.json" \
+	--data-binary "@./demorec_easi_5min.json" \
 	--output "./eval_plot_response.png" \
-	"http://192.168.80.102:50100/v1/evaluation/plot"
+	"https://api.cardio.ai/v1/evaluation/plot"
