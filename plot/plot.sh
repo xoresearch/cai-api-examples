3b69274104591e4e3df334923437977c8e3c5b64
danek <danek.kulik@icloud.com>
2018-06-24 10:37:20 +0300

    add demo record with single channel (lead1)

--- plot/plot.sh
+++ plot/plot.sh
@@ -6,6 +6,6 @@ curl \
 	--request POST \
 	--header "X-Api-Key: your-api-key" \
 	--header "Content-Type: application/json; charset=utf-8" \
-	--data-binary "@$(dirname "${0}")/../records/demorec_easi_5min.json" \
+	--data-binary "@$(dirname "${0}")/../records/demorec_lead1_5min.json" \
 	--output "./plot_response.png" \
 	"https://stage.web.cardio.ai/v1/plot"
--- recognize/recognize.sh
+++ recognize/recognize.sh
@@ -6,5 +6,5 @@ curl \
 	--request POST \
 	--header "X-Api-Key: your-api-key" \
 	--header "Content-Type: application/json; charset=utf-8" \
-	--data-binary "@$(dirname "${0}")/../records/demorec_easi_5min.json" \
+	--data-binary "@$(dirname "${0}")/../records/demorec_lead1_5min.json" \
 	"https://stage.web.cardio.ai/v1/recognize"
