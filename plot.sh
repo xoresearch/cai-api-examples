e5ad68244961790cc13966ef75884a66cd72c2ba
danek-kulik <danek.kulik@icloud.com>
2018-11-02 05:21:15 +0200

    moved all the scripts into the root directory

--- plot.sh
+++ plot.sh
@@ -4,7 +4,7 @@ FILE_NAME="12_channels_82sec.scp"
 #FILE_NAME="8_channels_10sec.scp"
 #FILE_NAME="3_channels_5min.json"
 
-FILE_PATH=$(dirname "${0}")/../records/${FILE_NAME}
+FILE_PATH=$(dirname "${0}")/records/${FILE_NAME}
 
 # Make sure the input file exists
 if [ ! -f ${FILE_PATH} ]; then
@@ -16,13 +16,13 @@ fi
 CONTENT_TYPE=""
 
 case ${FILE_NAME} in
-	*.json)
+	*.JSON|*.json)
 		CONTENT_TYPE="application/json; charset=utf-8"
 		;;
-	*.edf)
+	*.EDF|*.edf)
 		CONTENT_TYPE="application/edf"
 		;;
-	*.scp)
+	*.SCP|*.scp)
 		CONTENT_TYPE="application/scp"
 		;;
 	*)
@@ -38,4 +38,5 @@ curl \
 	--request POST \
 	--header "Content-Type: ${CONTENT_TYPE}" \
 	--data-binary "@${FILE_PATH}" \
-	"https://api.cardio.ai/v1/classify"
+	--output "./plot_response.png" \
+	"https://api.cardio.ai/v1/plot"
